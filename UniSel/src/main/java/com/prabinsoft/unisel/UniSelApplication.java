0d7f443a71b79cbbb176cfdd212d4a2710aab7a2
Prabin-sc1 <prabin.sc1@gmail.com>
2025-05-24 01:46:47 +0545

    first commit

---
+++ UniSel/src/main/java/com/prabinsoft/unisel/UniSelApplication.java
@@ -0,0 +1,13 @@
+package com.prabinsoft.unisel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniSelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniSelApplication.class, args);
+	}
+
+}
